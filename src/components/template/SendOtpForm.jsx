34ebd2d7fd1896acc71a456371a9cbb2e53202f4
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-21 02:37:47 +0330

    create service folder with auth.js servise to send phone number to server and use this in sendOtpForm

---
+++ src/components/template/SendOtpForm.jsx
@@ -0,0 +1,31 @@
+import { sendOtp } from "../../services/auth"
+
+function SendOtpForm({ mobile, setMobile, setStep }) {
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+
+        // validation //libery
+        if(mobile.length !== 11)  return
+
+        const { response, error } = await sendOtp(mobile)
+        console.log( response, error);
+
+        // if(response) {
+        //     setStep(2)
+        // } else {
+        //     console.log(error.response.data.message);
+        // }
+
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <p>ورود به حساب کاربری</p>
+            <label htmlFor="inputNumber">شماره موبایل خود را وارد کنید</label>
+            <input type="number" id="inputNumber" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
+            <button type="submit">ارسال کد تایید</button>
+        </form>
+    )
+}
+
+export default SendOtpForm
---
+++ src/services/auth.js
@@ -0,0 +1,13 @@
+import api from "../configs/API"
+
+const sendOtp = async (mobile) => {
+    try {
+        const response = await api.post('auth/send-otp', { mobile })
+        return { response }
+
+    } catch (error) {
+        return { error }
+ 
+    }
+}
+export { sendOtp }
