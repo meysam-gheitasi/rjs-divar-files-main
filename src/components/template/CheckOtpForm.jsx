09a653380af7c9e5e32fd10b7f111767c971f847
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-21 02:57:53 +0330

    create form send user code

--- src/components/template/CheckOtpForm.jsx
+++ src/components/template/CheckOtpForm.jsx
@@ -1,8 +1,19 @@
 
 
-function CheckOtpForm() {
+function CheckOtpForm({code, setCode, mobile, setStep}) {
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(mobile, code);
+  }
+
   return (
-    <div>CheckOtpForm</div>
+    <form onSubmit={submitHandler}>
+      <p>تایید کد ارسال شده</p>
+      <label htmlFor="inputCode">کد ارسال شده به شماره {mobile} را وارد کنید</label>
+      <input type="number" id="inputCode" placeholder="کد را وارد کنید" value={code} onChange={ e => setCode(e.target.value)} />
+      <button type="submit">ورود</button>
+    </form>
   )
 }
 
