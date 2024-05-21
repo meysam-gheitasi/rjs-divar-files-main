12574a570cb4836eb93603fe0572c686c0ebfe72
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-21 17:37:21 +0330

    use methode setCookie .

--- src/components/template/CheckOtpForm.jsx
+++ src/components/template/CheckOtpForm.jsx
@@ -1,16 +1,22 @@
 import { checkOtp } from "../../services/auth"
 
+import { setCookie } from '../../utils/cookie'
 
-function CheckOtpForm({code, setCode, mobile, setStep}) {
+
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
 
   const submitHandler = async (e) => {
     e.preventDefault()
-    
-     // validation //libery
-    if(code.toString().length !== 5) return
 
-    const {response, error} = await checkOtp(mobile, code)
-    console.log(response, error);
+    // validation //libery
+    if (code.toString().length !== 5) return
+
+    const { response, error } = await checkOtp(mobile, code)
+    if (response) {
+      setCookie(response.data)
+    } else {
+      console.log(error.response.data.message);
+    }
   }
 
   return (
@@ -18,8 +24,8 @@ function CheckOtpForm({code, setCode, mobile, setStep}) {
       <h4>ورود به حساب کاربری</h4>
       <p>کد تأیید را وارد کنید</p>
       <label htmlFor="inputCode">کد پیامک‌شده به شمارۀ {mobile} .را وارد کنید</label>
-      <input type="number" id="inputCode" placeholder="کد را وارد کنید" value={code} onChange={ e => setCode(e.target.value)} />
-      <button onClick={()=> setStep(1) }>تغییر شمارهٔ موبایل</button>
+      <input type="number" id="inputCode" placeholder="کد را وارد کنید" value={code} onChange={e => setCode(e.target.value)} />
+      <button onClick={() => setStep(1)}>تغییر شمارهٔ موبایل</button>
       <button>درخواست کد</button>
       <button type="submit">ورود</button>
     </form>
