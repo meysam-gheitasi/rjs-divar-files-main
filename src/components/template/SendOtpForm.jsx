555764178d2a534d9eb51003500a56979b895a07
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-21 17:20:54 +0330

    change template HTML form in login component.

--- src/components/template/SendOtpForm.jsx
+++ src/components/template/SendOtpForm.jsx
@@ -6,11 +6,11 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
         e.preventDefault()
 
         // validation //libery
-        if(mobile.length !== 11)  return
+        if (mobile.length !== 11) return
 
         const { response, error } = await sendOtp(mobile)
-    
-        if(response) {
+
+        if (response) {
             setStep(2)
         } else {
             console.log(error.response.data.message);
@@ -18,12 +18,27 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
 
     }
     return (
-        <form onSubmit={submitHandler}>
-            <p>ورود به حساب کاربری</p>
-            <label htmlFor="inputNumber">شماره موبایل خود را وارد کنید</label>
-            <input type="number" id="inputNumber" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
-            <button type="submit">ارسال کد تایید</button>
-        </form>
+            <div className="log-modal">
+                <header className="log-modal_header">
+                    <div className="log-modal_title-box">ورود به حساب کاربری</div>
+                    <button className="log-modal_close-button">icon</button>
+                </header>
+                <div className="log-modal_contants">
+                    <div className="log-modal_body">
+                        <div className="auth-content">
+                            <p className="auth-content__title">شمارهٔ موبایل خود را وارد کنید</p>
+                            <p className="auth-content__message">برای استفاده از امکانات دیوار، لطفاً شمارهٔ موبایل خود را وارد کنید. کد تأیید به این شماره پیامک خواهد شد.</p>
+                            <form onSubmit={submitHandler}>
+                                <input type="number" id="inputNumber" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
+                                <label htmlFor="inputNumber"><a href="">شرایط استفاده از خدمات</a> و <a href="/">حریم خصوصی</a> دیوار را می‌پذیرم.</label>
+                                <div className="">
+                                    <button type="submit">تایید</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 }
 
