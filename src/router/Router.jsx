870c309f57fc46bc2b6e52a52e4ac8da4aea074a
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-24 01:58:44 +0330

    create config react query in useQuery hock to send token user and get data response as server and with isLoading useQuery.

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,8 +4,12 @@ import HomePage from "pages/HomePage";
 import AdminPage from "pages/AdminPage";
 import NotfoundPage from "pages/404";
 import DashboardPage from "pages/DashboardPage";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "src/services/user";
 
 function Router() {
+    const {data, isLoading, error} = useQuery(["profile"], getProfile)
+    console.log(data, isLoading, error)
     return (
         <Routes>
             <Route index element={<HomePage />} />
