916899b880825221820627a23f61993a783ba60f
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-24 21:49:26 +0330

    create how can  access to routes in Router component

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "pages/AuthPage";
 import HomePage from "pages/HomePage";
 import AdminPage from "pages/AdminPage";
@@ -8,16 +8,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "src/services/user";
 
 function Router() {
-    const {data, isLoading, error} = useQuery(["profile"], getProfile)
+    const { data, isLoading, error } = useQuery(["profile"], getProfile)
     console.log(data, isLoading, error)
 
-    // if (isLoading) return <h1>Loading...</h1>
+    if (isLoading) return <h1>Loading...</h1>
     return (
         <Routes>
             <Route index element={<HomePage />} />
-            <Route path="/auth" element={<AuthPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/auth" element={data ? <Navigate to="/dashboard" /> : <AuthPage />} />
+            <Route path="/admin" element={data && data.data.role === "ADMIN" ? <AdminPage /> : <Navigate to="/" />} />
+            <Route path="/dashboard" element={data ? <DashboardPage /> : <Navigate to="/auth" />} />
             <Route path="*" element={<NotfoundPage />} />
         </Routes>
     )
