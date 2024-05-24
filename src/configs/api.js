9ba40162a0a6218ef5b3edd1d144f0d6ca31f45f
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-24 03:51:19 +0330

    create new  interceptors to check response is it login token ? or no with refresh token get new login token

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,19 +1,35 @@
 import axios from "axios";
-import { getCookie } from "src/utils/cookies";
+import { getNewToken } from "src/services/token";
+import { getCookie, setCookie } from "src/utils/cookies";
 
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {'Content-Type': 'application/json'}
+    headers: { 'Content-Type': 'application/json' }
 })
 
-api.interceptors.request.use((request)=> {
+api.interceptors.request.use((request) => {
     const accessToken = getCookie("accessToken")
-    if(accessToken)
+    if (accessToken)
         request.headers["Authorization"] = `bearer ${accessToken}`
     return request
 }, (error) => {
     return Promise.reject(error)
 })
+
+api.interceptors.response.use((response) => {
+    return response
+},
+    async (error) => {
+        const orginalRequest = error.config
+        if (error.response.status === 401 && !orginalRequest._retry) {
+            orginalRequest._retry = true;
+            const res = await getNewToken()
+            if(!res?.response) return 
+            setCookie(res.response.data)
+            console.log('this is new LOG:', api());
+            return api(orginalRequest)
+        }
+    })
 
 export default api
