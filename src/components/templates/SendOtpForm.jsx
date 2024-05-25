fd00a09b99621d6311176d70c07d6c0ede7c8e1e
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-25 05:36:26 +0330

    create class style to component SendOtpForm.

--- src/components/templates/SendOtpForm.jsx
+++ src/components/templates/SendOtpForm.jsx
@@ -19,21 +19,24 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
 
     }
     return (
-            <div className="log-modal" >
-                <header className="log-modal_header">
+            <div className={styles.logModal} >
+                <header className={styles.logModalHeader}>
                     <div className="log-modal_title-box">ورود به حساب کاربری</div>
                     <button className="log-modal_close-button">icon</button>
                 </header>
-                <div className="log-modal_contants">
-                    <div className="log-modal_body">
-                        <div className="auth-content">
-                            <p className="auth-content__title">شمارهٔ موبایل خود را وارد کنید</p>
-                            <p className="auth-content__message">برای استفاده از امکانات دیوار، لطفاً شمارهٔ موبایل خود را وارد کنید. کد تأیید به این شماره پیامک خواهد شد.</p>
-                            <form onSubmit={submitHandler}>
+                <div className={styles.logModalContants}>
+                    <div className={styles.logModalBody}>
+                        <div className={styles.authContent}>
+                            <p className={styles.authContentTitle}>شمارهٔ موبایل خود را وارد کنید</p>
+                            <p className={styles.authContentMessage}>برای استفاده از امکانات دیوار، لطفاً شمارهٔ موبایل خود را وارد کنید. کد تأیید به این شماره پیامک خواهد شد.</p>
+                            <form onSubmit={submitHandler} className={styles.form}>
+                                <div className={styles.formInput}>
                                 <input type="number" id="inputNumber" placeholder="شماره موبایل" value={mobile} onChange={e => setMobile(e.target.value)} />
+                                <p>+98</p>
+                                </div>
                                 <label htmlFor="inputNumber"><a href="">شرایط استفاده از خدمات</a> و <a href="/">حریم خصوصی</a> دیوار را می‌پذیرم.</label>
-                                <div className="">
-                                    <button type="submit">تایید</button>
+                                <div className={styles.formBtnContainer}>
+                                    <button type="submit" className={styles.formBtn}>تایید</button>
                                 </div>
                             </form>
                         </div>
