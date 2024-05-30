5ebe72c22355253e9a90c48e9f89d871549b22b8
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-31 00:00:27 +0330

    create servise function to get categoreis

--- src/services/admin.js
+++ src/services/admin.js
@@ -2,4 +2,6 @@ import api from "src/configs/api"
 
 const addCategory = data => api.post('category', data)
 
-export { addCategory }
+const getCategories = () => api.get('category')
+
+export { addCategory, getCategories }
