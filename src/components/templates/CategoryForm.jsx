dfbd2c1e9fe45493a9eb0ba292c7102f27f297e6
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-30 19:41:56 +0330

    create form elements with satet form and submitHandler and hock useMutation()

---
+++ src/components/templates/CategoryForm.jsx
@@ -0,0 +1,38 @@
+import { useMutation } from "@tanstack/react-query"
+import { useState } from "react"
+import { addCategory } from "src/services/admin"
+
+
+function CategoryForm() {
+
+    const [form, setForm] = useState({ name: '', slug: '', icon: '' })
+
+    const { data, error, isLoading, isSuccess, mutate } = useMutation(addCategory)
+
+    console.log({data, error, isLoading, isSuccess});
+
+    const changeHandler = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        mutate(form);
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandler} onChange={changeHandler}>
+                <label htmlFor="name">نام دسته بندی</label>
+                <input type="text" name="name" id="name" placeholder="" />
+                <label htmlFor="slug">اسلاگ دسته بندی</label>
+                <input type="text" name="slug" id="slug" placeholder="" />
+                <label htmlFor="icon">ایکون دسته بندی</label>
+                <input type="text" name="icon" id="icon" placeholder="" />
+                <button onSubmit={submitHandler}></button>
+            </form>
+        </div>
+    )
+}
+
+export default CategoryForm
