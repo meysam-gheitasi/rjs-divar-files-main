97bf239b13ebc5080090714ed3c376689732234a
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-30 19:52:39 +0330

    error handeling

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -9,7 +9,7 @@ function CategoryForm() {
 
     const { data, error, isLoading, isSuccess, mutate } = useMutation(addCategory)
 
-    console.log({data, error, isLoading, isSuccess});
+    console.log({ data, error, isLoading, isSuccess });
 
     const changeHandler = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
@@ -17,11 +17,14 @@ function CategoryForm() {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        mutate(form);
+        if (!form.name || !form.slug || !form.icon) return
+            mutate(form);
     }
 
     return (
         <div>
+            {isSuccess && <p>دسته بندی با موفقیت ایجاد شد </p>}
+            {!!error && <p>مشکلی پیش آمده است</p>}
             <form onSubmit={submitHandler} onChange={changeHandler}>
                 <label htmlFor="name">نام دسته بندی</label>
                 <input type="text" name="name" id="name" placeholder="" />
