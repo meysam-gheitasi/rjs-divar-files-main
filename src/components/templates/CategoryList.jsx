2fe38627b117bb8117040dfe56ae80c0570b05e2
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-31 01:41:53 +0330

    get and show gategories with use deleted methode.

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,9 +1,31 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { deleteCategory, getCategories } from "src/services/admin"
 
 
 function CategoryList() {
-  return (
-    <div>CategoryList</div>
-  )
+
+    const { data, isLoading } = useQuery(['get-categories'], getCategories)
+
+    const queryClient = useQueryClient()
+
+    const deleteHandler = async (id) => {
+       const response =  await deleteCategory(id)
+       if(!!response) {
+        await queryClient.invalidateQueries(
+           { queryKey: ['get-categories'],}
+        )
+       }
+    }
+
+    console.log(data, isLoading);
+    return (
+        <div>{data ? data.data.map(item => <div key={item._id}>
+            <h5>{item.name}</h5>
+            <p>{item.slug}</p>
+            <img src={`${item.icon}.svg`} />
+            <button onClick={() => deleteHandler(item._id)}>حذف</button>
+        </div>) : ''}</div>
+    )
 }
 
 export default CategoryList
