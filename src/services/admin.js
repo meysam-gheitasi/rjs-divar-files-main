2d0db6821b2fb1aca2472038819b64b6de98d0e3
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-31 01:44:58 +0330

    can be used delet categorie whit id in url request.

--- src/services/admin.js
+++ src/services/admin.js
@@ -4,6 +4,13 @@ const addCategory = data => api.post('category', data)
 
 const getCategories = () => api.get('category')
 
-const deleteCategory = (id) => api.delete('category', id)
+const deleteCategory = async (id) => {
+    try {
+        const response = await api.delete(`category/${id}`)
+        return { response }
+    } catch (error) {
+        return { error }
+    }
+}
 
 export { addCategory, getCategories, deleteCategory }
