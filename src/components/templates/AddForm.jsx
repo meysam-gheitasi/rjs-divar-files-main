7fb868767ffb8a6932b467ab5be2cd91c86f53be
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-05-31 15:15:02 +0330

    create elements form to geat data create form with admin.

--- src/components/templates/AddForm.jsx
+++ src/components/templates/AddForm.jsx
@@ -1,8 +1,39 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { getCategories } from "src/services/admin"
 
 function AddForm() {
-  return (
-    <div>AddForm</div>
-  )
+
+    const { data, isLoading } = useQuery(['get-categories'], getCategories)
+
+    const submitHandler = e => {
+        e.preventDefault()
+        console.log('send');
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <h3>افزودن آگهی</h3>
+                <label htmlFor="title">عنوان</label>
+                <input type="text" name="title" id="title" />
+                <label htmlFor="description"></label>
+                <textarea name="description" id="description" rows={10} cols={30} />
+                <label htmlFor="amount">قیمت</label>
+                <input type="text" name="amount" id="amount" />
+                <label htmlFor="city">شهر</label>
+                <input type="text" name="city" id="city" />
+                <select name="category" id="category">
+                    {data?.data.map(item => <option key={item._id} value={item._id}>
+                        {item.name}
+                    </option>)}
+                </select>
+                <label htmlFor="image">عکس</label>
+                <input type="file" name="image" id="image" />
+                <button onSubmit={submitHandler}>ایجاد</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddForm
