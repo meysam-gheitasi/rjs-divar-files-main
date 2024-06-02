0cdb1466bb114b3d3bab5c8a08ce49942847caf4
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-02 16:27:50 +0330

    use style in compoennt Sidebar.

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCategories } from "src/services/admin"
+import styles from './Sidebar.module.css'
 
 function Sidebar() {
 
     const { data } = useQuery(['get-categories'], getCategories)
 
   return (
-        <div>
+        <div className={styles.sidebar}>
             <h4>تمام دسته بندی ها</h4>
            <ul>
            {data?.data.map(item => (
