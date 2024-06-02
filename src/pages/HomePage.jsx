490403f9eddb5a13977ca5719a453aca70fb651e
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-02 17:08:06 +0330

    get all data as useQuery in this component and send data to chailds.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import { useQuery } from "@tanstack/react-query"
+import Loader from "src/components/modules/Loader"
+import Main from "src/components/templates/Main"
+import Sidebar from "src/components/templates/Sidebar"
+import { getCategories } from "src/services/admin"
+import { getAllPosts } from "src/services/user"
+
+const container = { display: 'flex' }
 
 function HomePage() {
+
+  const { data: categories, isLoading: categoriesLoading } = useQuery(['get-categories'], getCategories)
+  const { data: allPosts, isLoading: postsLoading } = useQuery(['get-all-posts'], getAllPosts)
+
+
+
   return (
-    <div>HomePage</div>
+    <>
+      {categoriesLoading || postsLoading 
+      ?
+      <Loader />
+      :
+        <div style={container}>
+          <Sidebar categories={categories} />
+          <Main allPosts={allPosts} />
+        </div>
+      }
+    </>
   )
 }
 
