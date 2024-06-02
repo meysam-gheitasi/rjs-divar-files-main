d41b4a00ef139943cc70f1ce709cc5f68a0f47bf
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-02 19:20:17 +0330

    fix error eslint.

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,6 +1,7 @@
 
 import styles from './Sidebar.module.css'
 
+// eslint-disable-next-line react/prop-types
 function Sidebar({ categories }) {
 
 
@@ -9,6 +10,7 @@ function Sidebar({ categories }) {
         <div className={styles.sidebar}>
             <h4>تمام دسته بندی ها</h4>
             <ul>
+                 {/* eslint-disable-next-line react/prop-types */}
                 {categories.data.map(item => (
                     <li key={item._id}>
                         <img src={`${item.icon}.svg`} alt={`${item.name}`} />
