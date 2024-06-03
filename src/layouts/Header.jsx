8c399480ad3ecba6278f76bc9b4f8d3cab21d4cc
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-04 00:37:39 +0330

    use tag a in btn to header site,

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -8,47 +8,61 @@ function Header() {
       </a>
       <hr />
       <button className={styles.svgIcon}>
-        <img src="../public/assets/location.svg" alt="location" />
-        <span>
-          تهران
-        </span>
+        <a href="/">
+          <img src="../public/assets/location.svg" alt="location" />
+          <span>
+            تهران
+          </span>
+        </a>
       </button>
       <button className={styles.svgIcon}>
-        <span>
-          دسته ها
-        </span>
-        <img src="../public/assets/caret-down.svg" alt="caret-down" />
+        <a href="/">
+          <span>
+            دسته ها
+          </span>
+          <img src="../public/assets/caret-down.svg" alt="caret-down" />
+        </a>
       </button>
       <div className={styles.searchBox}>
         <input type="text" name="" id="" placeholder='جستجو در همهً آگهی ها' />
         <img src="../public/assets/search.svg" alt="search" />
       </div>
       <button className={styles.svgIcon}>
-        <img src="../public/assets/user.svg" alt="user" />
-        <span>
-          دیوار من
-        </span>
+        <a href="/">
+          <img src="../public/assets/user.svg" alt="user" />
+          <span>
+            دیوار من
+          </span>
+        </a>
       </button>
       <button className={styles.svgIcon}>
-        <img src="../public/assets/chat.svg" alt="user" />
-        <span>
-          چت
-        </span>
+        <a href="/">
+          <img src="../public/assets/chat.svg" alt="user" />
+          <span>
+            چت
+          </span>
+        </a>
       </button>
       <button className={styles.svgIcon}>
-        <img src="../public/assets/patch-question.svg" alt="user" />
-        <span>
-          پشتیبانی
-        </span>
+        <a href="/">
+          <img src="../public/assets/patch-question.svg" alt="user" />
+          <span>
+            پشتیبانی
+          </span>
+        </a>
       </button>
       <button className={styles.svgIcon}>
-        <img src="../public/assets/globe-americas.svg" alt="user" />
-        <span>
-          Fa
-        </span>
+        <a href="/">
+          <img src="../public/assets/globe-americas.svg" alt="user" />
+          <span>
+            Fa
+          </span>
+        </a>
       </button>
       <div className={styles.advertBtn}>
-        <button type="submit" className={styles.formBtn}>ثبت آگهی</button>
+        <a href="/">
+          <button type="submit" className={styles.formBtn}>ثبت آگهی</button>
+        </a>
       </div>
     </div>
   )
