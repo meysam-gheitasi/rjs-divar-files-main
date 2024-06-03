d75238ca0980650af6d822d97d8a34cf053ab7b3
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-04 01:19:33 +0330

    created new rout page to special page creatre new Ad users.

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,6 +7,7 @@ import DashboardPage from "pages/DashboardPage";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "src/services/user";
 import Loader from "src/components/modules/Loader";
+import AddForm from "src/components/templates/AddForm";
 
 function Router() {
     const { data, isLoading } = useQuery(["profile"], getProfile)
@@ -18,6 +19,7 @@ function Router() {
             <Route path="/auth" element={data ? <Navigate to="/dashboard" /> : <AuthPage />} />
             <Route path="/admin" element={data && data.data.role === "ADMIN" ? <AdminPage /> : <Navigate to="/" />} />
             <Route path="/dashboard" element={data ? <DashboardPage /> : <Navigate to="/auth" />} />
+            <Route path="/registrationAD" element={data ? <AddForm /> : <Navigate to="/auth" />} />
             <Route path="*" element={<NotfoundPage />} />
         </Routes>
     )
