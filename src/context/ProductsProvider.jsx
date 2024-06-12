507fc7cc453d421ac8ff33999115696a161964be
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-13 03:19:25 +0330

    created useStates for search value input box element in navbar, displayed state to show data in home page and query state object to have eny option to search and shoa data for user.

--- src/context/ProductsProvider.jsx
+++ src/context/ProductsProvider.jsx
@@ -1,13 +1,18 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 
 function ProductsProvider({ children }) {
 
     const ProductsProvider = createContext()
-    
+
+    const [search, setSerach] = useState("")
+
+    const [displayed, setDisplayed] = useState([])
+
+    const { query, setQuery } = useState({})
 
     return (
-        <ProductsProvider.Provider >
+        <ProductsProvider.Provider value={{search, setSerach, displayed, setDisplayed, query, setQuery}} >
             {children}
         </ProductsProvider.Provider>
     )
