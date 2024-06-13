9baabc9b6e72dc24c2be899197e565a2ae25a906
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-13 20:52:50 +0330

    development useEffect =>use and  exact route data with optional chaining send to methode searchInPosts and set call back this methode in setDisplayed and eventually show in home page posts.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react"
 import Loader from "src/components/modules/Loader"
 import Main from "src/components/templates/Main"
 import Sidebar from "src/components/templates/Sidebar"
+import { useValue } from "src/context/ProductsProvider"
 import { getCategories } from "src/services/admin"
 import { getAllPosts } from "src/services/user"
+import { searchInPosts } from "src/utils/searchs"
 
 const container = { display: 'flex' }
 
@@ -13,16 +15,23 @@ function HomePage() {
   const { data: categories, isLoading: categoriesLoading } = useQuery(['get-categories'], getCategories)
   const { data: allPosts, isLoading: postsLoading } = useQuery(['get-all-posts'], getAllPosts)
 
-  const [ displayed, setDisplayed ] = useState([])
+  const [displayed, setDisplayed] = useState([])
 
-  useEffect(() =>  {setDisplayed(allPosts)}, [allPosts])
+  const { query, setQuery } = useValue()
+
+  useEffect(() => { 
+    setDisplayed(allPosts) 
+    let finalPosts = searchInPosts(allPosts?.data?.posts, query.search)
+    setDisplayed(finalPosts)
+
+  }, [allPosts, displayed, query])
 
   return (
     <>
-      {categoriesLoading || postsLoading || !!displayed.length
-      ?
-      <Loader />
-      :
+      {categoriesLoading || postsLoading || !displayed
+        ?
+        <Loader />
+        :
         <div style={container}>
           <Sidebar categories={categories} />
           <Main allPosts={displayed} />
