94063803b3bd079cd205a546fe4abef4f4318aad
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-15 06:57:12 +0330

    creating explanatory text.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
 import Loader from "src/components/modules/Loader"
 import Main from "src/components/templates/Main"
 import Sidebar from "src/components/templates/Sidebar"
@@ -12,12 +13,18 @@ const container = { display: 'flex' }
 
 function HomePage() {
 
+  // gate cashing data categorys AND all posts as react-query.
   const { data: categories, isLoading: categoriesLoading } = useQuery(['get-categories'], getCategories)
   const { data: allPosts, isLoading: postsLoading } = useQuery(['get-all-posts'], getAllPosts)
 
+  // to display posts in searching and non-searching or category conditions.
   const [displayed, setDisplayed] = useState([])
 
+  // get state context with this method.
   const { query, setQuery } = useValue()
+
+  // use this state as react-router-dom to read and write to URL with query search.
+  const [searchParams, setSearchParams] = useSearchParams()
 
   useEffect(() => { 
     setDisplayed(allPosts) 
