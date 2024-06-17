61d0de0d7c750f25c2b0d3e8b01a84f8540d154f
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-17 19:22:10 +0330

    change tempelete element in button and image and slug.

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -11,20 +11,22 @@ function CategoryList() {
     const queryClient = useQueryClient()
 
     const deleteHandler = async (id) => {
-       const response =  await deleteCategory(id)
-       if(!!response) {
-        await queryClient.invalidateQueries(
-           { queryKey: ['get-categories'],}
-        )
-       }
+        const response = await deleteCategory(id)
+        if (!!response) {
+            await queryClient.invalidateQueries(
+                { queryKey: ['get-categories'], }
+            )
+        }
     }
 
     return (
         <div className={styles.list}>{data ? data.data.map(item => <div key={item._id}>
             <h5>{item.name}</h5>
-            <p>{item.slug}</p>
-            <img src={`${item.icon}.svg`} />
-            <button onClick={() => deleteHandler(item._id)}>حذف</button>
+            <span>
+                <p>{item.slug}</p>
+                <img src={`${item.icon}.svg`} />
+                <button onClick={() => deleteHandler(item._id)}>حذف</button>
+            </span>
         </div>) : ''}</div>
     )
 }
